e214d81ea15685710287e8cf6da3087071440862
jon <jon>
2022-03-15 14:19:06 -0500

    testing cleanup

--- test.clj
+++ test.clj
@@ -42,14 +42,12 @@
 
 ((fn (message) (println message))  "Hello world!")
 
-(apply println '(1 2 3 4))    ;; same as  (f 1 2 3 4)
-(apply println 1 '(2 3 4))    ;; same as  (f 1 2 3 4)
-(apply println 1 2 '(3 4))    ;; same as  (f 1 2 3 4)
-(apply println 1 2 3 '(4))    ;; same as  (f 1 2 3 4)
+(unless (= 10 (apply + '(1 2 3 4))) (println "FAILED 1"))
+(unless (= 10 (apply + 1 '(2 3 4))) (println "FAILED 2"))
+(unless (= 10 (apply + 1 2 '(3 4))) (println "FAILED 3"))
+(unless (= 10 (apply + 1 2 3 '(4))) (println "FAILED 4"))
 
 (println (let (a 1 b 2) (+ a b)))
-
-(println "factorials")
 
 ; defs are not variables, don't do this:
 (defn factorial-using-do-dotimes [n]
@@ -86,26 +84,24 @@
 (defn factorial-using-eval-and-cons [n]
   (eval (cons '* (range 1 (inc n)))))
 
-(println "do-times       " (factorial-using-do-dotimes 5))
-(println "do-while       " (factorial-using-do-while 5))
-(println "apply-range    " (factorial-using-apply-range 5))
-(println "apply-iterate  " (factorial-using-apply-iterate 5))
-(println "reduce         " (factorial-using-reduce 5))
-(println "make           " (fac5))
-(println "eval           " (factorial-using-eval-and-cons 5))
-(println "map            " (apply * (take 4 (map inc '(1 2 3 4 5)))))
+(unless (= 120 (factorial-using-do-dotimes 5)) (println "FAILED: factorial-using-do-dotimes"))
+(unless (= 120 (factorial-using-do-while 5)) (println "FAILED: factorial-using-do-while"))
+(unless (= 120 (factorial-using-apply-range 5)) (println "FAILED: factorial-using-apply-range"))
+(unless (= 120 (factorial-using-apply-iterate 5)) (println "FAILED: factorial-using-apply-iterate"))
+(unless (= 120 (factorial-using-reduce 5)) (println "FAILED: factorial-using-reduce"))
+(unless (= 120 (fac5)) (println "FAILED: fac5"))
+(unless (= 120 (factorial-using-eval-and-cons 5)) (println "FAILED: factorial-using-eval-and-cons"))
+(unless (= 120 (apply * (take 4 (map inc '(1 2 3 4 5))))) (println "FAILED: factorial map"))
 
-(defn padding-right [s width pad] 
-  (apply str (take width (concat s (repeat pad)))))
+(defn padding-right [s width pad] (apply str (take width (concat s (repeat pad)))))
+(unless (= "Clojure   " (padding-right "Clojure" 10 " ")) (println "FAILED: padding-right"))
 
-(println "'" (padding-right "Clojure" 10 " ") "'")
-
-(println (reduce + '(1 2 3 4 5)))  ;;=> 15
-(println (reduce + '()))           ;;=> 0
-(println (reduce + '(1)))          ;;=> 1
-(println (reduce + '(1 2)))        ;;=> 3
-(println (reduce + 1 '()))         ;;=> 1
-(println (reduce + 1 '(2 3)))      ;;=> 6
+(unless (= 15 (reduce + '(1 2 3 4 5))) (println "reduce 1 failed"))
+(unless (= 0 (reduce + '())) (println "reduce 2 failed"))
+(unless (= 1 (reduce + '(1))) (println "reduce 3 failed"))
+(unless (= 3 (reduce + '(1 2))) (println "reduce 4 failed"))
+(unless (= 1 (reduce + 1 '())) (println "reduce 5 failed"))
+(unless (= 6 (reduce + 1 '(2 3))) (println "reduce 6 failed"))
 
 (unless (= (distinct '(1 2 1 3 1 4 1 5)) '(1 2 3 4 5)) (println "distinct failed 1"))
 (unless (= (distinct '(1 2 1 3 1 4 1 5)) (range 1 6)) (println "distinct failed 2"))
