9ba0a004eb99a7bf31ee715e9e61ea8ce6c46fe3
Jon Olick <jon.olick@Jons-MacBook-Pro.local>
2022-03-23 23:43:11 -0500

    fixed count-lines

--- count_lines.clj
+++ count_lines.clj
@@ -1,6 +1,6 @@
 (println "Number of lines of code: " (count (split-lines (str
-    (slurp jo_lisp.cpp) 
-    (slurp jo_lisp_lazy.h) 
-    (slurp jo_lisp_math.h) 
-    (slurp jo_lisp_string.h) 
-    (slurp jo_lisp_system.h)))))
+    (slurp 'jo_lisp.cpp) 
+    (slurp 'jo_lisp_lazy.h) 
+    (slurp 'jo_lisp_math.h) 
+    (slurp 'jo_lisp_string.h) 
+    (slurp 'jo_lisp_system.h)))))
