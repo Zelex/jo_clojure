c77c19cf341f2a462fe3edaf70cf8e4d925323ec
jon <jon>
2022-06-23 17:58:45 -0500

    made formatting prettier

--- examples/jacobi.clj
+++ examples/jacobi.clj
@@ -1,10 +1,10 @@
 ; Solving for Ax=b via the jacobi method
 
 ; A is a diagonally dominant matrix
-(def A (matrix 4 4 [10., -1., 2., 0.,
-                    -1., 11., -1., 3.,
-                    2., -1., 10., -1.,
-                    0.0, 3., -1., 8.]))
+(def A (matrix 4 4 [10 -1  2  0,
+                    -1 11 -1  3,
+                     2 -1 10 -1,
+                     0  3 -1  8]))
 ; initial value for x is all zeros
 (def x (matrix 1 4 [0.0, 0.0, 0.0, 0.0]))
 (def b (matrix 1 4 [6., 25., -11., 15.]))
