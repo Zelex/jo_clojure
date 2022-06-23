67fc820f6e163eaa1643aab3051b99cb1ae6ec25
jon <jon>
2022-06-23 15:58:43 -0500

    cleaned up jacobi example

--- examples/jacobi.clj
+++ examples/jacobi.clj
@@ -1,6 +1,6 @@
 ; Solving for Ax=b via the jacobi method
-; A is a diagonally dominant matrix
 
+; A is a diagonally dominant matrix
 (def A (matrix 4 4 [10., -1., 2., 0.,
                     -1., 11., -1., 3.,
                     2., -1., 10., -1.,
@@ -15,16 +15,13 @@
 
 (println "System:")
 (println "A =" A)
-(println "D-vec =" D-vec)
-(println "D-mat =" D-mat)
-(println "R =" R)
-(println)
+(println "b =" b)
 
 (defn jacobi-iteration [x]
     (Math/matrix-div (Math/matrix-sub b (Math/matrix-mul R x)) D-mat))
 
-; Take the 100th iteration as the solutio
-(println "Solution is:" (take 1 (drop 50 (iterate jacobi-iteration x))))
+; Take the 100th iteration as the solution
+(println "x =" (take 1 (drop 50 (iterate jacobi-iteration x))))
 
 
 
