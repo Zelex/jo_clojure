43018793770266704b51a0d7932e5bb7bd2520ec
Jon Olick <>
2022-11-14 10:03:19 -0600

    some improvements to poly_solve example

--- examples/poly_solve.clj
+++ examples/poly_solve.clj
@@ -1,19 +1,18 @@
-;(def num-elems 1090)
-(def data-file 'poly_solve_2.csv)
-(def num-elems 4096)
-(def num-coefs 8)
+(def data-file 'poly_solve_1.csv)
+(def rows (map float (split-lines (slurp data-file))))
+(def num-elems (- (count rows) 1))
+(def num-coefs 10)
 
 ; Fill A with poly elems
-; i^5 i^4 i^3 i^2 i^1 1 where i is == row / 1089.0
 (def A (atom (matrix num-coefs num-elems)))
 (doall (for [i (range 0 num-elems) 
         :let [ii (/ i (- num-elems 1.0))
-              row [(Math/pow ii 7) (Math/pow ii 6) (Math/pow ii 5) (Math/pow ii 4) (Math/pow ii 3) (Math/pow ii 2) ii 1]]]
-            (swap! A matrix/set-row i row)))
+              row [(Math/pow ii 9) (Math/pow ii 8) (Math/pow ii 7) (Math/pow ii 6) (Math/pow ii 5) (Math/pow ii 4) (Math/pow ii 3) (Math/pow ii 2) ii 1]
+              ]]
+            (swap! A matrix/set-row i (drop (- 10 num-coefs) row))))
 
 ; Fill b with colums of values from poly solve data file
 (def b (atom (matrix 1 num-elems)))
-(def rows (map float (split-lines (slurp data-file))))
 (swap! b matrix/set-col 0 rows)
 
 ; Invert A
