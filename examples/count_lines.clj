b41a71a3ef9cf9e61f1f5573c21b972e7c414643
Jon Olick <>
2022-11-25 21:33:05 -0600

    added a missing file to count lines example

--- examples/count_lines.clj
+++ examples/count_lines.clj
@@ -1,6 +1,7 @@
 (println "Number of lines of code: " (count (split-lines (str
     (slurp 'jo_basic.cpp) 
     (slurp 'jo_basic_async.h)
+    (slurp 'jo_basic_b64.h)
     (slurp 'jo_basic_canvas.h)
     (slurp 'jo_basic_gif.h)
     (slurp 'jo_basic_io.h)
