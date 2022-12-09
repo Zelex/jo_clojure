8724f5e2bbb51fc4b7a95e41dafbb53a0f583378
Jon Olick <jon@JonEpic2.localdomain>
2022-12-09 16:19:20 -0600

    linux build fix

--- c.sh
+++ c.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 # https://stackoverflow.com/questions/3466166/how-to-check-if-running-in-cygwin-mac-or-linux/27776822
 
@@ -10,6 +10,14 @@ if [ "$(uname)" == "Darwin" ]; then
      -Os -fno-exceptions -lpthread -lobjc \
      -framework Cocoa -framework QuartzCore -framework OpenGL -framework Metal -framework MetalKit \
      -L/opt/homebrew/opt/mysql-client/lib \
+     -lmysqlclient \
+     -o basic
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    c++ -std=c++17 \
+     -DNO_SOKOL \
+     jo_basic.cpp \
+     imgui/imgui.cpp imgui/imgui_widgets.cpp imgui/imgui_draw.cpp imgui/imgui_tables.cpp imgui/imgui_demo.cpp \
+     -Os -fno-exceptions -lpthread -lobjc \
      -lmysqlclient \
      -o basic
 fi
