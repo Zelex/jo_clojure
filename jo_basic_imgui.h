d9a7bb527f811c362bd7c24d2d5b0a9e08459281
Jon Olick <>
2022-12-28 20:14:04 -0600

    imgui/set-next-window-pos, imgui/set-next-window-size

--- jo_basic_imgui.h
+++ jo_basic_imgui.h
@@ -34,10 +34,44 @@ static node_idx_t native_imgui_menu_item(env_ptr_t env, list_ptr_t args) {
     return NIL_NODE;
 }
 
+static node_idx_t native_imgui_set_next_window_pos(env_ptr_t env, list_ptr_t args) {
+    list_t::iterator it(args);
+    ImVec2 pos = ImVec2(get_node_float(*it++), get_node_float(*it++));
+    int flags = 0;
+    for(; it; ++it) {
+        jo_string str = get_node_string(*it);
+        if(str == "none")            flags |= ImGuiCond_None;
+        else if(str == "always")     flags |= ImGuiCond_Always;
+        else if(str == "once")       flags |= ImGuiCond_Once;
+        else if(str == "first-use-ever")  flags |= ImGuiCond_FirstUseEver;
+        else if(str == "appearing")  flags |= ImGuiCond_Appearing;
+    }
+    ImGui::SetNextWindowPos(pos, flags);
+    return NIL_NODE;
+}
+
+static node_idx_t native_imgui_set_next_window_size(env_ptr_t env, list_ptr_t args) {
+    list_t::iterator it(args);
+    ImVec2 pos = ImVec2(get_node_float(*it++), get_node_float(*it++));
+    int flags = 0;
+    for(; it; ++it) {
+        jo_string str = get_node_string(*it);
+        if(str == "none")            flags |= ImGuiCond_None;
+        else if(str == "always")     flags |= ImGuiCond_Always;
+        else if(str == "once")       flags |= ImGuiCond_Once;
+        else if(str == "first-use-ever")  flags |= ImGuiCond_FirstUseEver;
+        else if(str == "appearing")  flags |= ImGuiCond_Appearing;
+    }
+    ImGui::SetNextWindowSize(pos, flags);
+    return NIL_NODE;
+}
+
 void jo_basic_imgui_init(env_ptr_t env) {
 	env->set("imgui/main-menu-bar", new_node_native_function("imgui/main-menu-bar", &native_imgui_main_menu_bar, true, NODE_FLAG_PRERESOLVE));
 	env->set("imgui/menu-bar", new_node_native_function("imgui/menu-bar", &native_imgui_menu_bar, true, NODE_FLAG_PRERESOLVE));
 	env->set("imgui/menu", new_node_native_function("imgui/menu", &native_imgui_menu, true, NODE_FLAG_PRERESOLVE));
 	env->set("imgui/menu-item", new_node_native_function("imgui/menu-item", &native_imgui_menu_item, true, NODE_FLAG_PRERESOLVE));
+	env->set("imgui/set-next-window-pos", new_node_native_function("imgui/set-next-window-pos", &native_imgui_set_next_window_pos, false, NODE_FLAG_PRERESOLVE));
+	env->set("imgui/set-next-window-size", new_node_native_function("imgui/set-next-window-size", &native_imgui_set_next_window_size, false, NODE_FLAG_PRERESOLVE));
 }
 
